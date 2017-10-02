162efb8e02183eeb59b8c1300736cbb52f76fa68
Sergey Svirsky <zversergey@tut.by>
2017-10-02 23:30:47 +0300

    Avalonia Initialize

---
+++ Src/Untech.Home.App/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Untech.Home.App
+{
+	public class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			this.AttachDevTools();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+			var expensesGrid = this.FindControl<Grid>("Expense");
+			var savingGrid = this.FindControl<Grid>("Saving");
+			var incomeGrid = this.FindControl<Grid>("Income");
+
+			expensesGrid.ColumnDefinitions.
+
+		}
+	}
+}
