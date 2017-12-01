820bd4f6578d9738395cd3dfc1ce3009fdbd0804
Sergey Svirsky <zversergey@tut.by>
2017-12-01 22:17:06 +0300

    Update BuildWebHost to use command line args

--- Src/Untech.Home.Web/Program.cs
+++ Src/Untech.Home.Web/Program.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Untech.FinancePlanner.Data;
 using Untech.FinancePlanner.Data.Initializations;
 
@@ -13,10 +15,19 @@ namespace Untech.Home.Web
 			BuildWebHost(args).Run();
 		}
 
-		public static IWebHost BuildWebHost(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
+		public static IWebHost BuildWebHost(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("hosting.json", optional: true)
+				.AddCommandLine(args)
+				.Build();
+
+			return WebHost.CreateDefaultBuilder(args)
+				.UseConfiguration(config)
 				.UseStartup<Startup>()
 				.Build();
+		}
 
 		public static void EnsureDatabaseCreated()
 		{
