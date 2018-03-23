a59b912c6554b26c79a83a7d67788a8c9bf597b5
Sergey Svirsky <zversergey@tut.by>
2018-03-23 22:00:54 +0300

    GTD

---
+++ Src/Untech.ThingsPlanner.Domain/Model/Thing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Untech.Practices.DataStorage;
+
+namespace Untech.ThingsPlanner.Domain.Model
+{
+	public class Thing : IAggregateRoot
+	{
+		public int Key { get; }
+
+		public string Title { get; }
+
+	}
+
+}
