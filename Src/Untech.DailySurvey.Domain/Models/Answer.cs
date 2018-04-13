925b31c7992a4b511b535a4afeaaec797c589f4c
Sergey Svirsky <zversergey@tut.by>
2018-04-13 16:27:16 +0300

    Some changes

---
+++ Src/Untech.DailySurvey.Domain/Models/Answer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Untech.Practices.DataStorage;
+
+namespace Untech.DailySurvey.Domain.Models
+{
+	[DataContract]
+	public class Answer : IAggregateRoot
+	{
+		public Answer(int key, int questionKey, DateTime when, string value)
+		{
+			Key = key;
+			QuestionKey = questionKey;
+			When = when;
+			Value = value;
+		}
+
+		[DataMember]
+		public int Key { get; private set; }
+
+		[DataMember]
+		public int QuestionKey { get; private set; }
+
+		[DataMember]
+		public DateTime When { get; private set; }
+
+		[DataMember]
+		public string Value { get; private set; }
+	}
+}
---
+++ Src/Untech.DailySurvey.Domain/Models/Question.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Untech.Practices.DataStorage;
+
+namespace Untech.DailySurvey.Domain.Models
+{
+	[DataContract]
+	public class Question : IAggregateRoot
+	{
+		public Question(int key, string text)
+		{
+			Key = key;
+			Text = text;
+		}
+
+		[DataMember]
+		public int Key { get; private set; }
+
+		[DataMember]
+		public string Text { get; private set; }
+
+		[DataMember]
+		public bool IsClosed { get; set; }
+
+		[DataMember]
+		public bool IsObsolete { get; set; }
+
+		[DataMember]
+		public IReadOnlyCollection<string> Options { get; set; }
+	}
+}
