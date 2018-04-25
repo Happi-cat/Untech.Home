5640269ea96e977482a730c6f5fb063b286ce559
Sergey Svirsky <svirski.siarhei@gmail.com>
2018-04-25 10:22:04 +0300

    ux (#16)
    * monthly calendar range
    * updated financial journal month report

--- Src/Untech.Home.Web/ClientApp/financial-planner/monthly-report/ReportTable.tsx
+++ Src/Untech.Home.Web/ClientApp/financial-planner/monthly-report/ReportTable.tsx
@@ -1,44 +1,79 @@
 import * as React from 'react';
-import { IMonthlyFinancialReport, IMonthlyFinancialReportDayEntry } from '../api/Models';
-import { MoneyView } from '../components';
+import {IMonthlyFinancialReport, IMonthlyFinancialReportDayEntry} from '../api/Models';
+import {MoneyView, MonthView} from '../components';
+import {IMonthlyFinancialReportDay} from "../api";
+import './ReportTable.less';
 
 interface IMonthlyReportTableProps {
   report: IMonthlyFinancialReport;
-  onTaxonClick(taxonId : number): void;
+
+  onTaxonClick(taxonId: number): void;
 }
 
 export class MonthlyReportTable extends React.Component<IMonthlyReportTableProps, {}> {
   public render() {
-    return <div>
-      {this.props.report.days.map(reportDay => <div key={reportDay.day}>
-        <h3><span>{reportDay.day}</span>
-          <span> - </span>
-          <MoneyView amount={reportDay.actualTotals.amount} currencyCode={reportDay.actualTotals.currency.id} />
-          <span> / </span>
-          <MoneyView amount={reportDay.forecastedTotals.amount} currencyCode={reportDay.forecastedTotals.currency.id} />
-        </h3>
+    const { year, month } = this.props.report;
+    return <table className="monthly-table-report">
+      <thead>
+        <tr>
+          <th>
+            Category
+          </th>
+          <th>
+            Remarks
+          </th>
+          <th>
+            Actual
+          </th>
+          <th>
+            Forecasted
+          </th>
+        </tr>
+      </thead>
 
-        <ul>
-          {reportDay.entries.map((entry, index) => this.renderEntry(entry, index))}
-        </ul>
-      </div>)}
-    </div>;
+      {this.props.report.days.map(reportDay => this.renderDay(year, month, reportDay))}
+    </table>;
+  }
+
+  renderDay(year: number, month: number, day: IMonthlyFinancialReportDay) {
+
+    return <tbody key={day.day}>
+    <tr className="monthly-table-report-day">
+      <td colSpan={2}>
+        <MonthView year={year} month={month} />
+        {day.day}
+      </td>
+      <td>
+        <MoneyView amount={day.actualTotals.amount} currencyCode={day.actualTotals.currency.id}/>
+      </td>
+      <td>
+        <MoneyView amount={day.forecastedTotals.amount} currencyCode={day.forecastedTotals.currency.id}/>
+      </td>
+    </tr>
+
+    {day.entries.map((entry, index) => this.renderEntry(entry, index))}
+    </tbody>
   }
 
   renderEntry(entry: IMonthlyFinancialReportDayEntry, index: number) {
-    let { actual, forecasted } = entry;
-    actual = actual || { amount: 0, currency: { id: 'BYN' } };
-    forecasted = forecasted || { amount: 0, currency: { id: 'BYN' } };
+    let {actual, forecasted} = entry;
+    actual = actual || {amount: 0, currency: {id: 'BYN'}};
+    forecasted = forecasted || {amount: 0, currency: {id: 'BYN'}};
 
 
-    return <li key={index} onClick={() => this.handleMonthClick(entry.taxonKey)}>
-      <span>{entry.name}</span><span> - </span>
-      <span>{entry.remarks}</span>
-      <span> - </span>
-      <MoneyView amount={actual.amount} currencyCode={actual.currency.id} />
-      <span> / </span>
-      <MoneyView amount={forecasted.amount} currencyCode={forecasted.currency.id} />
-    </li>;
+    return <tr className="-hoverable" key={index} onClick={() => this.handleMonthClick(entry.taxonKey)}>
+      <td>
+        {entry.name}
+      </td>
+      <td>
+        {entry.remarks}</td>
+      <td>
+        <MoneyView amount={actual.amount} currencyCode={actual.currency.id}/>
+      </td>
+      <td>
+        <MoneyView amount={forecasted.amount} currencyCode={forecasted.currency.id}/>
+      </td>
+    </tr>;
   }
 
   handleMonthClick = (taxonId: number) => {
