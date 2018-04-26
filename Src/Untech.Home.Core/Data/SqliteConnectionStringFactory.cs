0f65b60b86002e40a4dc353078bd5ab9a5e01cf2
Sergey Svirsky <zversergey@tut.by>
2018-04-26 19:24:28 +0300

    fix null field

--- Src/Untech.Home.Core/Data/SqliteConnectionStringFactory.cs
+++ Src/Untech.Home.Core/Data/SqliteConnectionStringFactory.cs
@@ -5,18 +5,18 @@ namespace Untech.Home.Data
 {
 	public class SqliteConnectionStringFactory
 	{
-		private readonly string databaseFolder;
+		private readonly string _databaseFolder;
 
 		public SqliteConnectionStringFactory(string databaseFolder)
 		{
-
+			_databaseFolder = databaseFolder;
 		}
 
 		public string GetConnectionString(string fileName)
 		{
 			var builder = new DbConnectionStringBuilder
 			{
-				{ "Data Source", Path.Combine(databaseFolder, fileName) },
+				{ "Data Source", Path.Combine(_databaseFolder, fileName) },
 				{ "Pooling", "True" },
 				{ "Max Pool Size", 100 }
 			};
