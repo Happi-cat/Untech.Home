ede9576297950a7af391c1dd389f8b06b66fdb0b
Sergey Svirsky <zversergey@tut.by>
2018-04-26 11:52:08 +0300

    query

---
+++ Src/Untech.ThingsPlanner.Domain/Requests/ThingQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Untech.Practices.CQRS;
+using Untech.ThingsPlanner.Domain.Models;
+
+namespace Untech.ThingsPlanner.Domain.Requests
+{
+	public class ThingQuery : IQuery<Thing>
+	{
+
+	}
+
+	public class ThingsQuery : IQuery<IEnumerable<Thing>>
+	{
+		
+	}
+}
