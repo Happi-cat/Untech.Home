17f14ad4961d3e59866b79bc36b934855d9d185a
Sergey Svirsky <zversergey@tut.by>
2018-05-02 16:19:51 +0300

    Added commands & queries

---
+++ Src/Untech.Users.Domain/Requests/CreateUser.cs
@@ -0,0 +1,9 @@
+﻿using Untech.Practices.CQRS;
+
+namespace Untech.Users.Domain.Requests
+{
+	public class CreateUser : ICommand<User>
+	{
+		public int? TelegramId { get; set; }
+	}
+}
---
+++ Src/Untech.Users.Domain/Requests/FindUserByTelegramId.cs
@@ -0,0 +1,9 @@
+﻿using Untech.Practices.CQRS;
+
+namespace Untech.Users.Domain.Requests
+{
+	public class FindUserByTelegramId : IQuery<User>
+	{
+		public int TelegramId { get; set; }
+	}
+}
