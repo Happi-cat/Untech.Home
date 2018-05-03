fb3a51c9a59f87c24f7465e24800e9712f73fca4
Sergey Svirsky <svirski.siarhei@gmail.com>
2018-05-03 11:22:28 +0300

    users (#22)
    * Users module init

---
+++ Src/Untech.Users.Domain/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using Untech.Practices.DataStorage;
+
+namespace Untech.Users.Domain.Models
+{
+	[DataContract]
+	public class User : IAggregateRoot
+	{
+		/// <summary>
+		/// For deserializer
+		/// </summary>
+		private User()
+		{
+		}
+
+		public User(int key)
+		{
+			Key = key;
+		}
+
+		[DataMember]
+		public int Key { get; private set; }
+
+		[DataMember]
+		public int? TelegramId { get; set; }
+	}
+}
---
+++ Src/Untech.Users.Domain/Requests/CreateUser.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+using Untech.Practices.CQRS;
+using Untech.Users.Domain.Models;
+
+namespace Untech.Users.Domain.Requests
+{
+	[DataContract]
+	public class CreateUser : ICommand<User>
+	{
+		[DataMember]
+		public int? TelegramId { get; set; }
+	}
+}
