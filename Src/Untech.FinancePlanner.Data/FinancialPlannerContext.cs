02830d8a7a1aec6b555b9214c50b4e533a8dfcad
Sergey Svirsky <svirski.siarhei@gmail.com>
2018-05-09 11:55:26 +0300

    db init changes (#25)
    * IDbInitializer
    * DbInitializer -> TaxonTableInitializer

--- Src/Untech.FinancePlanner.Data/FinancialPlannerContext.cs
+++ Src/Untech.FinancePlanner.Data/FinancialPlannerContext.cs
@@ -2,11 +2,12 @@
 using LinqToDB.Data;
 using LinqToDB.DataProvider.SQLite;
 using Untech.FinancePlanner.Data.Cache;
+using Untech.Home;
 using Untech.Home.Data;
 
 namespace Untech.FinancePlanner.Data
 {
-	public class FinancialPlannerContext : DataConnection
+	public class FinancialPlannerContext : DataConnection, IDbInitializer
 	{
 		public FinancialPlannerContext(IConnectionStringFactory connectionStringFactory)
 			: base(new SQLiteDataProvider(), connectionStringFactory.GetConnectionString("financial_planner.db"))
@@ -18,5 +19,12 @@ namespace Untech.FinancePlanner.Data
 		public ITable<FinancialJournalEntryDao> FinancialJournalEntries => GetTable<FinancialJournalEntryDao>();
 
 		public ITable<TaxonDao> Taxons => GetTable<TaxonDao>();
+
+		public void InitializeDb()
+		{
+			this.EnsureTableExists<CacheEntry>();
+			this.EnsureTableExists<TaxonDao>();
+			this.EnsureTableExists<FinancialJournalEntryDao>();
+		}
 	}
 }
