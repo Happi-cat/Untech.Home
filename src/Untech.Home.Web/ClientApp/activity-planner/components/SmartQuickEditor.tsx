d0aa1145fdb4767137895abfe846bf8235f0b567
Sergey Svirsky <zversergey@tut.by>
2018-05-16 17:02:47 +0300

    Updated component to support promise

--- src/Untech.Home.Web/ClientApp/activity-planner/components/SmartQuickEditor.tsx
+++ src/Untech.Home.Web/ClientApp/activity-planner/components/SmartQuickEditor.tsx
@@ -1,24 +1,25 @@
 import * as React from 'react';
 import { QuickEditor } from "./QuickEditor";
-import { Button } from "semantic-ui-react";
+import { Button, Loader } from "semantic-ui-react";
 import './SmartQuickEditor.less';
 
 
 export interface ISmartQuickEditorProps {
   value: string;
-  onSave(value: string): void;
-  onDelete(): void;
+  onSave(value: string): Promise<void>;
+  onDelete(): Promise<void>;
 }
 
 export interface ISmartQuickEditorState {
   editMode: boolean;
+  isDeleting: boolean;
 }
 
 export class SmartQuickEditor extends React.Component<ISmartQuickEditorProps, ISmartQuickEditorState> {
   constructor(props: any) {
     super(props);
 
-    this.state = { editMode: false };
+    this.state = { editMode: false, isDeleting: false };
   }
 
   public render() {
@@ -28,13 +29,28 @@ export class SmartQuickEditor extends React.Component<ISmartQuickEditorProps, IS
       </div>
     }
 
+    if (this.state.isDeleting)
+    {
+      return <div>
+        <Loader active inline size={"small"}  >
+          Deleting
+        </Loader>
+      </div>
+    }
+
     return <div className="smart-quick-editor">
       <div className="smart-quick-editor__label">{this.props.value}</div>
       <Button.Group floated='right' size='mini'>
-        <Button onClick={this.props.onDelete} icon='trash' color='orange' />
+        <Button onClick={this.handleDelete} icon='trash' color='orange' />
         <Button onClick={this.handleEdit} icon='edit' positive />
       </Button.Group>
     </div>
+  }
+
+  handleDelete = async () => {
+    this.setState({ isDeleting: true });
+    await this.props.onDelete();
+    this.setState({ isDeleting: false });
   }
 
   handleEdit = () => {
