e26a7a25e54015b89f8d2840e7c4d6180ea4c7db
Sergey Svirsky <zversergey@tut.by>
2018-05-16 17:15:39 +0300

    index for components

---
+++ src/Untech.Home.Web/ClientApp/activity-planner/components/index.ts
@@ -0,0 +1,8 @@
+﻿import {QuickAdder} from "./QuickAdder";
+import {QuickEditor} from "./QuickEditor";
+import {HorScrollable} from "./HorScrollable";
+import {SmartQuickEditor} from "./SmartQuickEditor";
+
+export {
+  QuickAdder, QuickEditor, HorScrollable, SmartQuickEditor
+};
